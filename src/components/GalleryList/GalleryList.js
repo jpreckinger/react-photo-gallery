cf51685037819755ce4f8eff97334cc004ca990b
Jonathan Reckinger <jpreckinger@gmail.com>
2018-10-26 20:15:14 -0500

    branch created, component established and basic frame set up for work

---
+++ src/components/GalleryList/GalleryList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+class GalleryList extends Component {
+
+  render() {
+    return (
+        <p>Gallery goes here</p>
+    );
+  }
+}
+
+export default GalleryList;
