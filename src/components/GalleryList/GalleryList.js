5a5b9b1717a70ee856a226c6f5e83ee88b011008
Jonathan Reckinger <jpreckinger@gmail.com>
2018-10-27 13:03:30 -0500

    added feature that changes the text depending on the number of likes to be gramatically correct

--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -4,7 +4,20 @@ import './GalleryList.css';
 
 class GalleryList extends Component {
 
+    setText = (image) => {
+        let amountOfLove = '';
+        if(image === 0) {
+            amountOfLove = 'Nobody loves this =(';
+        } else if( image === 1){
+            amountOfLove = '1 person loves this.';
+        } else {
+            amountOfLove = image + ' people love this!';
+        }
+        return amountOfLove;
+    }
+
   render() {
+
     return (
         <div>
             {
@@ -13,7 +26,7 @@ class GalleryList extends Component {
                         <div className="column">
                             <GalleryItem galleryItem={image} />
                             <button onClick={() => this.props.loveIt(image.id)}>Love it!</button>
-                            <p>{image.likes} people love this!</p> 
+                            <p>{this.setText(image.likes)}</p> 
                         </div>
                     </section>
                 )
